5a51e66bb7f558454f8cf8c1d3a842be9a4998d4
CHONGMING-PC\zhangcm <itcto@139.com>
2024-09-23 13:23:04 +0800

    Changes

--- isite-commons/commons-cloud/src/main/java/org/isite/commons/cloud/constants/HttpHeaders.java
+++ isite-commons/commons-cloud/src/main/java/org/isite/commons/cloud/constants/HttpHeaders.java
@@ -1,17 +1,26 @@
 package org.isite.commons.cloud.constants;
+
 /**
- * @Description 自定义的请求头字段以x-打头
+ * @Description Http Header 常量类.自定义的请求头字段以x-打头
  * @Author <font color='blue'>zhangcm</font>
  */
-public class CloudConstants {
+public class HttpHeaders {
 
-    private CloudConstants() {
-    }
+    private HttpHeaders() {}
 
+    public static final String USER_AGENT = "User-Agent";
+    public  static final String CONNECTION = "Connection";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_TRANSFER_ENCODING = "Content-Transfer-Encoding";
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
+    public static final String X_FORWARDED_HOST = "x-forwarded-host";
+    public static final String AUTHORIZATION = "Authorization";
     /**
-     * 如果在请求头设置feign-sign-password，该拦截器自动设置签名信息。
+     * 用于识别协议（HTTP 或 HTTPS）
      */
-    public static final String FEIGN_SIGN_PASSWORD = "feign-sign-password";
+    public static final String X_FORWARDED_PROTO = "x-forwarded-proto";
+    public static final String X_FORWARDED_PREFIX = "x-forwarded-prefix";
+
 
     /**
      * 服务版本号，主要用于灰度发布：将部分流量引导到新版本的服务，同时保持大部分流量在旧版本上，从而进行逐步验证和测试
