70c412842267aa1d2d922d13e386b7c13ba5881d
CHONGMING-PC\zhangcm <itcto@139.com>
2024-09-26 11:48:12 +0800

    Changes

--- isite-jpa/jpa-support/src/main/java/org/isite/jpa/data/BuiltIn.java
+++ isite-jpa/jpa-support/src/main/java/org/isite/jpa/data/BuiltIn.java
@@ -1,7 +1,8 @@
 package org.isite.jpa.data;
 
 /**
- * @Description 系统内置数据，只能使用数据库脚本插入和删除，内置字段不能编辑（其他字段可以修改）
+ * @Description 系统内置数据模型父接口。系统内置数据，只能手动执行数据库脚本插入和删除，
+ * 不能使用接口新增和删除，接口不能更新内置字段（其他字段可以修改）
  * @Author <font color='blue'>zhangcm</font>
  */
 public interface BuiltIn {
