72a2cd2335937f3c6563abd99d9e1af6f7e18814
CHONGMING-PC\zhangcm <itcto@139.com>
2024-10-21 17:26:14 +0800

    Changes

--- isite-jpa/jpa-support/src/main/java/org/isite/jpa/data/Model.java
+++ isite-jpa/jpa-support/src/main/java/org/isite/jpa/data/Model.java
@@ -1,12 +1,10 @@
 package org.isite.jpa.data;
 
-import java.io.Serializable;
-
 /**
  * @Description 数据模型(PO)父接口
  * @Author <font color='blue'>zhangcm</font>
  */
-public interface Model<I> extends Serializable {
+public interface Model<I> {
     /**
      * 获取主键
      */
