8368b6bb8d33f2f1c36610c3d54d821bc6012b06
CHONGMING-PC\zhangcm <itcto@139.com>
2025-03-05 11:31:46 +0800

    Changes

---
+++ isite-commons/isite-commons-lang/src/main/java/org/isite/commons/lang/enums/ActiveStatusDeserializer.java
@@ -0,0 +1,7 @@
+package org.isite.commons.lang.enums;
+
+/**
+ * @Author <font color='blue'>zhangcm</font>
+ */
+public class ActiveStatusDeserializer extends EnumerableDeserializer<ActiveStatus> {
+}
--- isite-commons/isite-commons-lang/src/main/java/org/isite/commons/lang/enums/SwitchDeserializer.java
+++
@@ -1,7 +0,0 @@
-package org.isite.commons.lang.enums;
-
-/**
- * @author <font color='blue'>zhangcm</font>
- */
-public class SwitchDeserializer extends EnumConstantDeserializer<ActiveStatus> {
-}
